dc4b743031890941eb48cfae7f40e4410160fc94
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-09-18 18:54:39 +0100

    x

---
+++ uc-non-persistent/ansible/run.tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clear
+echo; echo "##############################################################################################################"
+echo
+
+echo
+echo "Placeholder script for running the tests ..."
+echo
+echo "please implement me!"
+echo
+
+###
+# The End.
