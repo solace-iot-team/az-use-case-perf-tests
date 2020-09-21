c04dc88020c69f33928b9a2ea2550415209fe5a8
Jochen Traunecker <jochen.traunecker@gmail.com>
2020-09-21 15:40:12 +0200

    added skdperf content
    bootstrapping sdkperf nodes, triggering load and latency tests

---
+++ uc-non-persistent/ansible/run.sdkperf.publisher.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------------------------
+# MIT License
+# Copyright (c) 2020, Solace Corporation, Ricardo Gomez-Ulmke (ricardo.gomez-ulmke@solace.com)
+# ---------------------------------------------------------------------------------------------
+clear
+echo; echo "##############################################################################################################"
+echo
+
+  ############################################################################################################################
+  # SELECT
+    # logging & debug: ansible
+    export ANSIBLE_LOG_PATH="./ansible.log"
+    export ANSIBLE_DEBUG=False
+    # logging: ansible-solace
+    export ANSIBLE_SOLACE_LOG_PATH="./ansible-solace.log"
+    export ANSIBLE_SOLACE_ENABLE_LOGGING=True
+
+    export ANSIBLE_HOST_KEY_CHECKING=False
+
+  # END SELECT
+
+##############################################################################################################################
+# Prepare
+
+
+# Run SDKPerf VM bootstrap
+
+  inventory="./inventory/inventory.json"
+  playbook="./sdkperf.publisher.start.playbook.yml"
+  privateKeyFile="../keys/azure_key"
+
+  ansible-playbook \
+                    -i $inventory \
+                    --private-key $privateKeyFile \
+                    $playbook \
+                    # -vvv
+
+  if [[ $? != 0 ]]; then echo ">>> ERROR. aborting."; echo; exit 1; fi
+
+
+
+
+###
+# The End.
