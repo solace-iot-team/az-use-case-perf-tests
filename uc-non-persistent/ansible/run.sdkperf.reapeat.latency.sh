8716bd06971a0f9a238781ef90b39cfe7d3c4587
Jochen Traunecker <jochen.traunecker@gmail.com>
2020-09-22 11:38:28 +0200

    added repeating latency runs

---
+++ uc-non-persistent/ansible/run.sdkperf.reapeat.latency.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------------------------
+# MIT License
+# Copyright (c) 2020, Solace Corporation, Ricardo Gomez-Ulmke (ricardo.gomez-ulmke@solace.com)
+# ---------------------------------------------------------------------------------------------
+clear
+echo; echo "##############################################################################################################"
+echo
+
+  ############################################################################################################################
+  # SELECT
+    # logging & debug: ansible
+    export ANSIBLE_LOG_PATH="./ansible.log"
+    export ANSIBLE_DEBUG=False
+    # logging: ansible-solace
+    export ANSIBLE_SOLACE_LOG_PATH="./ansible-solace.log"
+    export ANSIBLE_SOLACE_ENABLE_LOGGING=True
+
+    export ANSIBLE_HOST_KEY_CHECKING=False
+
+  # END SELECT
+
+##############################################################################################################################
+echo; 
+echo "###############################################################################################################"
+echo "#                                                                                                             #"
+echo "#    Starting Repeating SDKPerf Latency Testruns                                                              #"
+echo "#                                                                                                             #"                                                                              #"
+echo "#                                                                                                             #"
+echo "#                                                                                                             #"
+echo "###############################################################################################################"
+
+
+  inventory="./inventory/inventory.json"
+  playbook="./sdkperf.latency.repeat.playbook.yml"
+  privateKeyFile="../keys/azure_key"
+
+  ansible-playbook \
+                    -i $inventory \
+                    --private-key $privateKeyFile \
+                    $playbook \
+                    # -vvv
+
+  if [[ $? != 0 ]]; then echo ">>> ERROR. aborting."; echo; exit 1; fi
+
+
+
+
+###
+# The End.
