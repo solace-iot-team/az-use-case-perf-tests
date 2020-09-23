95cbfc8a3b8f8bbdac31602387a4b6721ae9d88e
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-09-23 09:44:12 +0100

    created

---
+++ uc-common/.lib/functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------------------------
+# Copyright (c) 2020, Solace Corporation, Ricardo Gomez-Ulmke (ricardo.gomez-ulmke@solace.com).
+# All rights reserved.
+# Licensed under the MIT License.
+# ---------------------------------------------------------------------------------------------
+
+function assertFile() {
+  if [[ $# -lt 1 ]]; then
+      echo "Usage: fileVar='\$(assertFile {full-path/filename})'" 1>&2
+      return 1
+  fi
+  local file=$1
+  if [[ ! -f "$file" ]]; then
+    echo ">>> ERR: file='$file' does not exist. aborting." > /dev/tty
+    echo > /dev/tty
+    return 1;
+  fi
+  echo $file
+  return 0
+}
+
+
+
+###
+# The End.
