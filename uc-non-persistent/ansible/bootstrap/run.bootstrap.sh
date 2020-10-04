25bf53d241b23384491ab335cbf4e4540d1f5da1
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-10-04 14:42:14 +0100

    new bootstrap directory

--- uc-non-persistent/ansible/bootstrap/run.bootstrap.sh
+++ uc-non-persistent/ansible/bootstrap/run.bootstrap.sh
@@ -16,7 +16,7 @@ echo
 
   export ANSIBLE_LOG_PATH="./ansible.log"
   export ANSIBLE_DEBUG=False
-  # export ANSIBLE_VERBOSITY=3
+  export ANSIBLE_VERBOSITY=0
   export ANSIBLE_HOST_KEY_CHECKING=False
   export ANSIBLE_SOLACE_LOG_PATH="./ansible-solace.log"
   export ANSIBLE_SOLACE_ENABLE_LOGGING=True
@@ -35,26 +35,27 @@ echo
   fi
 
 ##############################################################################################################################
-# Generate inventories
+# Generate inventory
 
   $scriptDir/../inventory/generate-with-sc.sh
   if [[ $? != 0 ]]; then echo ">>> ERROR generate inventories. aborting."; echo; exit 1; fi
 
-exit
 ##############################################################################################################################
 # Prepare
-inventory="./inventory/"$PERF_CLOUDPROVIDER".inventory.json"
-privateKeyFile="../keys/"$PERF_CLOUDPROVIDER"_key"
 
 rm -f ./*.log
 
+##############################################################################################################################
+# General for all playbooks
+inventoryFile=$(assertFile "$scriptDir/../inventory/$UC_NON_PERSISTENT_INFRASTRUCTURE.inventory.json") || exit
+cloudProvider=${UC_NON_PERSISTENT_INFRASTRUCTURE%%.*}
+privateKeyFile=$(assertFile "$projectHome/keys/"$cloudProvider"_key") || exit
 
 ##############################################################################################################################
 # Run SDKPerf VM bootstrap
-
-  playbook="./sdkperf.centos.bootstrap.playbook.yml"
+  playbook=$(assertFile "$scriptDir/sdkperf.centos.bootstrap.playbook.yml") || exit
   ansible-playbook \
-                    -i $inventory \
+                    -i $inventoryFile \
                     --private-key $privateKeyFile \
                     $playbook
   if [[ $? != 0 ]]; then echo ">>> ERROR. aborting."; echo; exit 1; fi
@@ -62,9 +63,9 @@ rm -f ./*.log
 ##############################################################################################################################
 # Run Broker VM bootstrap
 
-  playbook="./broker.centos.bootstrap.playbook.yml"
+  playbook=$(assertFile "$scriptDir/broker.centos.bootstrap.playbook.yml") || exit
   ansible-playbook \
-                    -i $inventory \
+                    -i $inventoryFile \
                     --private-key $privateKeyFile \
                     $playbook
   if [[ $? != 0 ]]; then echo ">>> ERROR. aborting."; echo; exit 1; fi
@@ -72,18 +73,24 @@ rm -f ./*.log
 ##############################################################################################################################
 # Run Broker PubSub bootstrap
 
-  playbook="./broker.pubsub.bootstrap.playbook.yml"
+  playbook=$(assertFile "$scriptDir/broker.pubsub.bootstrap.playbook.yml") || exit
   ansible-playbook \
-                    -i $inventory \
+                    -i $inventoryFile \
                     $playbook
   if [[ $? != 0 ]]; then echo ">>> ERROR. aborting."; echo; exit 1; fi
+
+# TODO:
+echo "!!!!!!!!!!!!!!"
+echo " !!!! check if the inits are still required here after changes in calling sdkperf method.."
+echo "!!!!!!!!!!!!!!"
+exit 1
 
 ##############################################################################################################################
 # Connect Consumer to Broker initially
 
-  playbook="./sdkperf.consumer.init.playbook.yml"
+  playbook=$(assertFile "$scriptDir/sdkperf.consumer.init.playbook.yml") || exit
   ansible-playbook \
-                    -i $inventory \
+                    -i $inventoryFile \
                     --private-key $privateKeyFile \
                     $playbook
   if [[ $? != 0 ]]; then echo ">>> ERROR. aborting."; echo; exit 1; fi
@@ -91,9 +98,9 @@ rm -f ./*.log
 ##############################################################################################################################
 # Connect Publisher once to Broker initially
 
-  playbook="./sdkperf.publisher.init.playbook.yml"
+  playbook=$(assertFile "$scriptDir/sdkperf.publisher.init.playbook.yml") || exit
   ansible-playbook \
-                    -i $inventory \
+                    -i $inventoryFile \
                     --private-key $privateKeyFile \
                     $playbook
   if [[ $? != 0 ]]; then echo ">>> ERROR. aborting."; echo; exit 1; fi
@@ -101,9 +108,9 @@ rm -f ./*.log
 ##############################################################################################################################
 # Connect Latency once to Broker initially
 
-  playbook="./sdkperf.latency.init.playbook.yml"
+  playbook=$(assertFile "$scriptDir/sdkperf.latency.init.playbook.yml") || exit
   ansible-playbook \
-                    -i $inventory \
+                    -i $inventoryFile \
                     --private-key $privateKeyFile \
                     $playbook
   if [[ $? != 0 ]]; then echo ">>> ERROR. aborting."; echo; exit 1; fi
