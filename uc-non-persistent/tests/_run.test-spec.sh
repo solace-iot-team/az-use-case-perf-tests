4480296253753b15665eaaf41ef90112ab71bac4
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-10-29 19:01:14 +0000

    llllllllllllllll

--- uc-non-persistent/tests/_run.test-spec.sh
+++ uc-non-persistent/tests/_run.test-spec.sh
@@ -33,8 +33,6 @@ source $projectHome/.lib/functions.sh
 ############################################################################################################################
 # Generate Run Specs
 
-exit 1
-
 playbook="$scriptDir/playbooks/run.test-spec.playbook.yml"
 ansible-playbook \
                 -i $TEST_SPEC_INVENTORY_FILE  \
