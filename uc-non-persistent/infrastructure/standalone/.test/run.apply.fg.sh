206766505287cd85b3b43c91d760a0fbf83fb1c6
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-10-30 13:02:52 +0000

    qqqqqqqqqqq

--- uc-non-persistent/infrastructure/standalone/.test/run.apply.fg.sh
+++ uc-non-persistent/infrastructure/standalone/.test/run.apply.fg.sh
@@ -11,7 +11,7 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
 #  format: {cloud_provider}.{config}
 export infrastructureIds=(
   "azure.test1"
-  "azure.test2"
+  # "azure.test2"
   "aws.test1"
 )
 
