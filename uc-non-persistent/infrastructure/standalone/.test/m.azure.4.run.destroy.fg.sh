de25d58ab3a4a3b53a071a5cfd2b6b2f52af32cd
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-10-30 09:08:31 +0000

    created, to run manual tests of 4 azure infras at the same time

---
+++ uc-non-persistent/infrastructure/standalone/.test/m.azure.4.run.destroy.fg.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# ---------------------------------------------------------------------------------------------
+# MIT License
+# Copyright (c) 2020, Solace Corporation, Ricardo Gomez-Ulmke (ricardo.gomez-ulmke@solace.com)
+# ---------------------------------------------------------------------------------------------
+
+scriptDir=$(cd $(dirname "$0") && pwd);
+scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
+
+
+#  format: {cloud_provider}.{config}
+export infrastructureIds=(
+  "azure.test1"
+  "azure.test2"
+  "azure.test3"
+  "azure.test4"
+)
+
+export INFRASTRUCTURE_IDS="${infrastructureIds[*]}"
+
+export LOG_DIR=$scriptDir/logs
+rm -f $LOG_DIR/**destroy**
+
+export TF_VARIABLES_DIR=$scriptDir
+
+../_run.destroy-all.sh > $LOG_DIR/$scriptName.out 2>&1
+
+###
+# The End.
