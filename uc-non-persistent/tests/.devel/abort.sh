cc0830641ea6076260541f52886b960d676b4469
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-10-31 14:52:16 +0000

    added for more devel convenience

--- uc-non-persistent/tests/.devel/abort.sh
+++ uc-non-persistent/tests/.devel/abort.sh
@@ -10,15 +10,15 @@ projectHome=${scriptDir%/uc-non-persistent/*}
 usecaseHome=$projectHome/uc-non-persistent
 
 export TMP_DIR="$scriptDir/../tmp"
-export TEST_SPEC_FILE="$scriptDir/1_test.test.spec.yml"
-export TEST_SPEC_INVENTORY_FILE="$TMP_DIR/test-specs/1_test.test.spec.inventory.yml"
+export TEST_SPEC_FILE="$scriptDir/devel1.test.spec.yml"
+export TEST_SPEC_INVENTORY_FILE="$TMP_DIR/test-specs/devel1.test.spec.inventory.yml"
 
 export SHARED_SETUP_DIR=$usecaseHome/shared-setup;
 
-export ANSIBLE_VERBOSITY=0
-nohup ../_abort.sh > ./logs/$scriptName.out 2>&1 &
-# export ANSIBLE_VERBOSITY=3
-# ../_abort.sh
+# export ANSIBLE_VERBOSITY=0
+# nohup ../_abort.sh > ./logs/$scriptName.out 2>&1 &
+export ANSIBLE_VERBOSITY=3
+../_abort.sh
 
 ###
 # The End.
