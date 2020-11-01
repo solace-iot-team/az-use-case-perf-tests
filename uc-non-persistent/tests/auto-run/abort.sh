aae0b8f65b5ad727e6a4c972c7ab10691675d208
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-11-01 09:30:56 +0000

    fixed abort script

--- uc-non-persistent/tests/auto-run/abort.sh
+++ uc-non-persistent/tests/auto-run/abort.sh
@@ -8,11 +8,11 @@ scriptDir=$(cd $(dirname "$0") && pwd);
 scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
 projectHome=${scriptDir%/uc-non-persistent/*}
 usecaseHome=$projectHome/uc-non-persistent
+export TMP_DIR="$scriptDir/../tmp"
 
 export TEST_SPEC_FILE="$scriptDir/1_auto.test.spec.yml"
 export TEST_SPEC_INVENTORY_FILE="$TMP_DIR/test-specs/1_auto.test.spec.inventory.yml"
 
-export TMP_DIR="$scriptDir/../tmp"
 export SHARED_SETUP_DIR=$usecaseHome/shared-setup;
 
 export ANSIBLE_VERBOSITY=0
