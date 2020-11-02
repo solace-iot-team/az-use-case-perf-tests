fd24f674556d6c4476f292288e25c02d988ff7bc
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-11-02 13:24:53 +0000

    vpn-stats enhancements start

--- uc-non-persistent/tests/run/monitors/.devel/run.monitor.vpn-stats.sh
+++ uc-non-persistent/tests/run/monitors/.devel/run.monitor.vpn-stats.sh
@@ -10,10 +10,10 @@ projectHome=${scriptDir%/uc-non-persistent/*}
 usecaseHome=$projectHome/uc-non-persistent
 
 
-export UC_NON_PERSISTENT_INFRASTRUCTURE="azure.test1-standalone"
-runName="1_variation"
+export UC_NON_PERSISTENT_INFRASTRUCTURE="aws.devel1-standalone"
+runName="vpn_stats"
 
-
+export ANSIBLE_VERBOSITY=3
 export RUN_SPEC_FILE="$usecaseHome/tests/tmp/run-specs/$UC_NON_PERSISTENT_INFRASTRUCTURE.$runName.yml"
 export SHARED_SETUP_DIR="$usecaseHome/shared-setup"
 export RUN_LOG_FILE_BASE="$usecaseHome/tests/tmp/$UC_NON_PERSISTENT_INFRASTRUCTURE.$runName"
