23fa2376f8df2c6d5c983bad1e26a540ca746aae
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-11-14 06:47:42 +0000

    prep for prod run workflow

---
+++ uc-non-persistent/infrastructure/standalone/auto-run/run.apply.fg.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# ---------------------------------------------------------------------------------------------
+# MIT License
+# Copyright (c) 2020, Solace Corporation, Ricardo Gomez-Ulmke (ricardo.gomez-ulmke@solace.com)
+# ---------------------------------------------------------------------------------------------
+
+scriptDir=$(cd $(dirname "$0") && pwd);
+scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
+
+
+#  format: {cloud_provider}.{config}
+export infrastructureIds=(
+  "azure.1-auto"
+  # "azure.2-auto"
+  "aws.1-auto"
+)
+
+export INFRASTRUCTURE_IDS="${infrastructureIds[*]}"
+
+export LOG_DIR=$scriptDir/logs
+rm -f $LOG_DIR/*
+
+export TF_VARIABLES_DIR=$scriptDir
+
+../_run.apply-all.sh > $LOG_DIR/$scriptName.out 2>&1
+
+
+###
+# The End.
