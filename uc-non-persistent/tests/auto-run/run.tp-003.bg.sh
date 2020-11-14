07ceac65070dfca74adce81bf067d1e95a9ab2c3
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-11-14 05:49:05 +0000

    added tp-003: soak test

---
+++ uc-non-persistent/tests/auto-run/run.tp-003.bg.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# ---------------------------------------------------------------------------------------------
+# MIT License
+# Copyright (c) 2020, Solace Corporation, Ricardo Gomez-Ulmke (ricardo.gomez-ulmke@solace.com)
+# ---------------------------------------------------------------------------------------------
+
+scriptDir=$(cd $(dirname "$0") && pwd);
+scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
+projectHome=${scriptDir%/uc-non-persistent/*}
+usecaseHome=$projectHome/uc-non-persistent
+
+export TEST_SPEC_FILE="$scriptDir/tp-003.test.spec.yml"
+
+export ANSIBLE_VERBOSITY=0
+
+nohup ../_run.sh > ./logs/$scriptName.out 2>&1 &
+
+###
+# The End.
