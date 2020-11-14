515dd04ad5806292d3fe9723bec5e6edfe0ba1b7
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-11-14 11:23:46 +0000

    added post load broekr vpn stats collection and analysis checks

--- analytics/.devel/run.bg.sh
+++ analytics/.devel/run.bg.sh
@@ -22,8 +22,8 @@ export TEST_RESULTS_DIR="$projectHome/uc-non-persistent/test-results/stats"
 export ANALYSIS_OUT_DIR="$projectHome/uc-non-persistent/test-results/analysis"
 
 infrastructureIds=(
-  "aws.devel1-standalone"
-  # "azure.devel1"
+  # "aws.devel1-standalone"
+  "azure.devel1-standalone"
   # "azure.devel2"
 )
 
