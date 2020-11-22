5c07b10241b7016b4ae853c68102e61cd4078ef4
ricardojosegomezulmke <ricardo.gomez-ulmke@solace.com>
2020-11-22 11:40:32 +0000

    switched validatio on

--- uc-non-persistent/tests/auto-run/run.tp-003.fg.sh
+++ uc-non-persistent/tests/auto-run/run.tp-003.fg.sh
@@ -11,6 +11,7 @@ usecaseHome=$projectHome/uc-non-persistent
 
 export TEST_SPEC_FILE="$scriptDir/tp-003.test.spec.yml"
 
+export VALIDATE_SPECS="True"
 export ANSIBLE_VERBOSITY=0
 
 ../_run.sh > ./logs/$scriptName.out 2>&1
